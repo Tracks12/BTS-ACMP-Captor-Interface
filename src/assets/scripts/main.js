ddd88a91131396eec28770e23aaf969256d60778
MENEGHE <meneghinieric@gmail.com>
2020-03-03 11:35:59 +0100

    Changement du css, du html et du js pour le graph particules

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -12,78 +12,56 @@ $(document).ready(() => {
 
 	// HIGH CHART CODE HERE
 
-  Highcharts.chart('container', {
+//js graphe particule fines
+var defaultData = 'https://demo-live-data.highcharts.com/time-data.csv'; //fichier qui recevra les données de la carte
+var urlInput = document.getElementById('fetchURL');
+var pollingCheckbox = document.getElementById('enablePolling');
+var pollingInput = document.getElementById('pollingTime');
 
-      title: {
-          text: 'Solar Employment Growth by Sector, 2010-2016'
-      },
+function createChart() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Live Data'
+        },
+        accessibility: {
+            announceNewData: {
+                enabled: true,
+                minAnnounceInterval: 15000,
+                announcementFormatter: function (allSeries, newSeries, newPoint) {
+                    if (newPoint) {
+                        return 'New point added. Value: ' + newPoint.y;
+                    }
+                    return false;
+                }
+            }
+        },
+        data: {
+            csvURL: urlInput.value,
+            enablePolling: pollingCheckbox.checked === true,
+            dataRefreshRate: parseInt(pollingInput.value, 10)
+        }
+    });
 
-      subtitle: {
-          text: 'Source: thesolarfoundation.com'
-      },
+    if (pollingInput.value < 1 || !pollingInput.value) {
+        pollingInput.value = 1;
+    }
+}
 
-      yAxis: {
-          title: {
-              text: 'Number of Employees'
-          }
-      },
+urlInput.value = defaultData;
 
-      xAxis: {
-          accessibility: {
-              rangeDescription: 'Range: 2010 to 2017'
-          }
-      },
+// We recreate instead of using chart update to make sure the loaded CSV
+// and such is completely gone.
+pollingCheckbox.onchange = urlInput.onchange = pollingInput.onchange = createChart;
 
-      legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'middle'
-      },
+// Create the chart
+createChart();
 
-      plotOptions: {
-          series: {
-              label: {
-                  connectorAllowed: false
-              },
-              pointStart: 2010
-          }
-      },
 
-      series: [{
-          name: 'Installation',
-          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-      }, {
-          name: 'Manufacturing',
-          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-      }, {
-          name: 'Sales & Distribution',
-          data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-      }, {
-          name: 'Project Development',
-          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-      }, {
-          name: 'Other',
-          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-      }],
+});// OU FAUT-IL METTRE CA?
 
-      responsive: {
-          rules: [{
-              condition: {
-                  maxWidth: 500
-              },
-              chartOptions: {
-                  legend: {
-                      layout: 'horizontal',
-                      align: 'center',
-                      verticalAlign: 'bottom'
-                  }
-              }
-          }]
-      }
-
-  });
-
-});
 
  var gaugeOptions = {
      chart: {
