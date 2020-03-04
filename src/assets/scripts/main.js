db62287b2bb32778cfe598331eb0a80c75faa24b
tracks12 <anarchy.dark97@gmail.com>
2020-03-04 11:00:08 +0100

    deleting parazit space

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,60 +6,59 @@
  * Location : /assets/scripts/
  */
 
- var gaugeOptions = {
- 	chart: {
- 		type: 'solidgauge'
- 	},
- 	title: null,
- 	pane: {
- 		center: ['50%', '85%'],
- 		size: '140%',
- 		startAngle: -90,
- 		endAngle: 90,
- 		background: {
- 			backgroundColor:
- 			Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
- 			innerRadius: '60%',
- 			outerRadius: '100%',
- 			shape: 'arc'
- 		}
- 	},
- 	exporting: {
- 		enabled: false
- 	},
- 	tooltip: {
- 		enabled: false
- 	},
+var gaugeOptions = {
+	chart: {
+		type: 'solidgauge'
+	},
+	title: null,
+	pane: {
+		center: ['50%', '85%'],
+		size: '140%',
+		startAngle: -90,
+		endAngle: 90,
+		background: {
+			backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+			innerRadius: '60%',
+			outerRadius: '100%',
+			shape: 'arc'
+		}
+	},
+	exporting: {
+		enabled: false
+	},
+	tooltip: {
+		enabled: false
+	},
 
- 	// the value axis
- 	yAxis: {
- 		stops: [
- 			[0.1, '#55BF3B'], // green
- 			[0.5, '#DDDF0D'], // yellow
- 			[0.9, '#DF5353'] // red
- 		],
- 		lineWidth: 0,
- 		tickWidth: 0,
- 		minorTickInterval: null,
- 		tickAmount: 2,
- 		title: {
- 			y: -70
- 		},
- 		labels: {
- 			y: 16
- 		}
- 	},
+	// the value axis
+	yAxis: {
+		stops: [
+			[0.1, '#55BF3B'], // green
+			[0.5, '#DDDF0D'], // yellow
+			[0.9, '#DF5353'] // red
+		],
+		lineWidth: 0,
+		tickWidth: 0,
+		minorTickInterval: null,
+		tickAmount: 2,
+		title: {
+			y: -70
+		},
+		labels: {
+			y: 16
+		}
+	},
 
- 	plotOptions: {
- 		solidgauge: {
- 			dataLabels: {
- 				y: 5,
- 				borderWidth: 0,
- 				useHTML: true
- 			}
- 		}
- 	}
- };
+	plotOptions: {
+		solidgauge: {
+			dataLabels: {
+				y: 5,
+				borderWidth: 0,
+				useHTML: true
+			}
+		}
+	}
+};
 
 $(document).ready(() => {
 	// Initialisation de la map
