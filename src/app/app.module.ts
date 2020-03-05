c68b4afb73a0ab0cc299f4b472c8e573a4c79dcd
tracks12 <anarchy.dark97@gmail.com>
2020-03-05 10:37:00 +0100

    Réorganisation de la page d'authentification avec la page admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import { AppComponent } from './app.component';
 
 // Importations des modules propres au site
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
 import { NavTopComponent } from './nav-top/nav-top.component';
 import { TelemetryComponent } from './telemetry/telemetry.component';
-import { AdminPaneComponent } from './admin-pane/admin-pane.component';
 import { CarbonComponent } from './telemetry/carbon/carbon.component';
 import { ParticulesComponent } from './telemetry/particules/particules.component';
 import { OzoneComponent } from './telemetry/ozone/ozone.component';
+import { AuthComponent } from './auth/auth.component';
+import { AdminComponent } from './auth/admin/admin.component';
 
 @NgModule({
 	imports: [
@@ -23,20 +23,19 @@ import { OzoneComponent } from './telemetry/ozone/ozone.component';
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		RouterModule.forRoot([
-			{ path: 'admin', component: AdminPaneComponent },
-			{ path: 'login', component: LoginComponent },
+			{ path: 'login', component: AuthComponent },
 			{ path: 'telemetry', component: TelemetryComponent }
 		])
 	],
 	declarations: [
 		AppComponent,
-		LoginComponent,
 		NavTopComponent,
 		TelemetryComponent,
-		AdminPaneComponent,
 		CarbonComponent,
 		ParticulesComponent,
 		OzoneComponent,
+		AuthComponent,
+		AdminComponent,
 	],
 	bootstrap: [
 		AppComponent
--- src/app/auth/admin/admin.component.spec.ts
+++ src/app/auth/admin/admin.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdminPaneComponent } from './admin-pane.component';
+import { AdminComponent } from './admin.component';
 
-describe('AdminPaneComponent', () => {
-  let component: AdminPaneComponent;
-  let fixture: ComponentFixture<AdminPaneComponent>;
+describe('AdminComponent', () => {
+  let component: AdminComponent;
+  let fixture: ComponentFixture<AdminComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminPaneComponent ]
+      declarations: [ AdminComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminPaneComponent);
+    fixture = TestBed.createComponent(AdminComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
