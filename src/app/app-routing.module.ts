f6b431bc1b829b934bf3bd3903d41af3bf4baa0d
tracks12 <anarchy.dark97@gmail.com>
2020-03-07 09:00:21 +0100

    Add login service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,5 +7,4 @@ const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
-export class AppRoutingModule { }
+}) export class AppRoutingModule { }
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
-})
-export class LoginService {
+	providedIn: 'root'
+}) export class LoginService {
+	entity = {};
 
-  constructor() { }
+	constructor(
+		private http: HttpClient
+	) { }
+
+	logIn() {
+
+	}
+
+	logOut() {
+
+	}
 }
