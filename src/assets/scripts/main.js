b9b4095294204899c9696f3abeef7260c3c2c54f
tracks12 <anarchy.dark97@gmail.com>
2020-04-04 15:23:49 +0200

    Restauration du fichier main.js du dossier src/scripts/assets/

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,116 +6,190 @@
  * Location : /assets/scripts/
  */
 
- var gaugeOptions = {
-     chart: {
-         type: 'solidgauge'
-     },
+var gaugeOptions = {
+	chart: {
+		type: 'solidgauge'
+	},
+	title: null,
+	pane: {
+		center: [ '50%', '85%' ],
+		size: '140%',
+		startAngle: -90,
+		endAngle: 90,
+		background: {
+			backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+			innerRadius: '60%',
+			outerRadius: '100%',
+			shape: 'arc'
+		}
+	},
+	exporting: {
+		enabled: false
+	},
+	tooltip: {
+		enabled: false
+	},
 
-     title: null,
+	// the value axis
+	yAxis: {
+		stops: [
+			[ 0.1, '#55BF3B' ], // green
+			[ 0.5, '#DDDF0D' ], // yellow
+			[ 0.9, '#DF5353' ] // red
+		],
+		lineWidth: 0,
+		tickWidth: 0,
+		minorTickInterval: null,
+		tickAmount: 2,
+		title: {
+			y: -70
+		},
+		labels: {
+			y: 16
+		}
+	},
 
-     pane: {
-         center: ['50%', '85%'],
-         size: '140%',
-         startAngle: -90,
-         endAngle: 90,
-         background: {
-             backgroundColor:
-                 Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
-             innerRadius: '60%',
-             outerRadius: '100%',
-             shape: 'arc'
-         }
-     },
+	plotOptions: {
+		solidgauge: {
+			dataLabels: {
+				y: 5,
+				borderWidth: 0,
+				useHTML: true
+			}
+		}
+	}
+};
 
-     exporting: {
-         enabled: false
-     },
+$(document).ready(() => {
+	// Splash Screen Animation
+	$('#splash').ready(() => {
+		$('#splash h1')
+			.delay(5000)
+			.fadeOut(1000)
+			.parent('div')
+			.delay(6000)
+			.fadeOut(500);
+	});
 
-     tooltip: {
-         enabled: false
-     },
+	// Initialisation de la map
+	$('#map').ready(() => mapInit());
 
-     // the value axis
-     yAxis: {
-         stops: [
-             [0.1, '#55BF3B'], // green
-             [0.5, '#DDDF0D'], // yellow
-             [0.9, '#DF5353'] // red
-         ],
-         lineWidth: 0,
-         tickWidth: 0,
-         minorTickInterval: null,
-         tickAmount: 2,
-         title: {
-             y: -70
-         },
-         labels: {
-             y: 16
-         }
-     },
+	// Fonction d'ouverture/fermeture du menu
+	$('#menu-content').ready(() => {
+		$('#menu-open').click(() => $('#menu-content').fadeIn());
+		$('#menu-close').click(() => $('#menu-content').fadeOut());
 
-     plotOptions: {
-         solidgauge: {
-             dataLabels: {
-                 y: 5,
-                 borderWidth: 0,
-                 useHTML: true
-             }
-         }
-     }
- };
+		$('.menu-interact-map').click(() => $('aside').css('display', 'none'));
+		$('.menu-interact-aside, .menu-interact-telemetry').click(() => $('aside').fadeIn());
+	});
 
- // The speed gauge
- var chartSpeed = Highcharts.chart('container-Co2', Highcharts.merge(gaugeOptions, {
-     yAxis: {
-         min: 0,
-         max: 200,
-         title: {
-             text: 'Co2'
-         }
-     },
+	// Affichage de la box si jamais il y a instance
+	$('aside').ready(() => {
+		let length = $(location).attr('pathname').split('/')[1].length;
 
-     credits: {
-         enabled: false
-     },
+		if(length > 0)
+			$('aside').fadeIn();
 
-     series: [{
-         name: 'Co2',
-         data: [80],
-         dataLabels: {
-             format:
-                 '<div style="text-align:center">' +
-                 '<span style="font-size:25px">{y}</span><br/>' +
-                 '<span style="font-size:12px;opacity:0.4">ppm</span>' +
-                 '</div>'
-         },
-         tooltip: {
-             valueSuffix: 'ppm'
-         }
-     }]
+		$('#aside-close').click(() => $('aside').fadeOut());
+	});
 
- }));
+	$('.menu-interact-telemetry').click(function() {
+		$(this).ready(() => {
+			//js graphe particule fines
+			var defaultData = 'https://demo-live-data.highcharts.com/time-data.csv'; //fichier qui recevra les données de la carte
+			var urlInput = $('#fetchURL')[0];
+			var pollingCheckbox = $('#enablePolling')[0];
+			var pollingInput = $('#pollingTime')[0];
 
- // Bring life to the dials
- setInterval(function () {
-     // Co2
-     var point,
-         newVal,
-         inc;
+			function createChart() {
+				Highcharts.chart('container', {
+					chart: {
+						type: 'spline'
+					},
+					title: {
+						text: 'Live Data'
+					},
+					accessibility: {
+						announceNewData: {
+							enabled: true,
+							minAnnounceInterval: 15000,
+							announcementFormatter: (allSeries, newSeries, newPoint) => {
+								if(newPoint)
+									return `New point added. Value: ${newPoint.y}`;
 
-     if (chartSpeed) {
-         point = chartSpeed.series[0].points[0];
-         inc = Math.round((Math.random() - 0.5) * 100);
-         newVal = point.y + inc;
+								return false;
+							}
+						}
+					},
+					data: {
+						csvURL: urlInput.value,
+						enablePolling: pollingCheckbox.checked === true,
+						dataRefreshRate: parseInt(pollingInput.value, 10)
+					}
+				});
 
-         if (newVal < 0 || newVal > 200) {
-             newVal = point.y - inc;
-         }
+				if(pollingInput.value < 1 || !pollingInput.value)
+					pollingInput.value = 1;
+			}
 
-         point.update(newVal);
-     }
- }, 2000);
+			urlInput.value = defaultData;
 
+			// We recreate instead of using chart update to make sure the loaded CSV
+			// and such is completely gone.
+			pollingCheckbox.onchange = urlInput.onchange = pollingInput.onchange = createChart;
+
+			// Create the chart
+			createChart();
+
+			// The Co2 gauge
+			var chartCo2 = Highcharts.chart('container-Co2', Highcharts.merge(gaugeOptions, {
+				yAxis: {
+					min: 0,
+					max: 200,
+					title: {
+						text: 'chartCo2'
+					}
+				},
+				credits: {
+					enabled: false
+				},
+				series: [{
+					name: 'co2',
+					data: [80],
+					dataLabels: {
+						format:
+							'<div style="text-align:center">' +
+								'<span style="font-size:25px">{y}</span><br/>' +
+								'<span style="font-size:12px;opacity:0.4">CO²</span>' +
+							'</div>'
+						},
+					tooltip: {
+						valueSuffix: 'Co2'
+					}
+				}]
+			}));
+
+			// Bring life to the dials
+			setInterval(() => {
+				// Speed
+				var point,
+					newVal,
+					inc;
+
+				if(chartSpeed) {
+					point = chartSpeed.series[0].points[0];
+					inc = Math.round((Math.random() - 0.5) * 100);
+					newVal = point.y + inc;
+
+					if (newVal < 0 || newVal > 200)
+						newVal = point.y - inc;
+
+					point.update(newVal);
+				}
+			}, 2000);
+		});
+	});
+});
 
 /**
  * END
